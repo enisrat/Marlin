8adb94f14fb7c415fccd083f9aef379439da2d69
Enisrat <84377236+enisrat@users.noreply.github.com>
2024-10-03 17:31:39 +0200

    feedrates

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1270,7 +1270,7 @@
  * Override with M203
  *                                      X, Y, Z [, I [, J [, K...]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_MAX_FEEDRATE          { 300, 300, 20, 45 }
+#define DEFAULT_MAX_FEEDRATE          { 300, 300, 25, 45 }
 
 //#define LIMITED_MAX_FR_EDITING        // Limit edit via M203 or LCD to DEFAULT_MAX_FEEDRATE * 2
 #if ENABLED(LIMITED_MAX_FR_EDITING)
@@ -1283,7 +1283,7 @@
  * Override with M201
  *                                      X, Y, Z [, I [, J [, K...]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_MAX_ACCELERATION      { 7000, 7000, 100, 8000 }
+#define DEFAULT_MAX_ACCELERATION      { 7000, 7000, 50, 8000 }
 
 //#define LIMITED_MAX_ACCEL_EDITING     // Limit edit via M201 or LCD to DEFAULT_MAX_ACCELERATION * 2
 #if ENABLED(LIMITED_MAX_ACCEL_EDITING)
